4bf1664b16457ec551331cf8726058e9d6eddfec
bachelier_mathis <06mathis08@gmail.com>
2024-03-21 12:06:18 +0100

    update

--- E-Commerce/app/models/Product.php
+++ E-Commerce/app/models/Product.php
@@ -18,5 +18,10 @@ class Product extends Model
         
         return $this->query("SELECT * FROM {$this->table} WHERE name LIKE ? OR id_category = ?",[$name, $category]);
     }
+    public function searchProductByNameAndCategory($search, $category){
+        
+        return $this->query("SELECT products.*, categories.name as category_name FROM products JOIN categories ON products.category_id = categories.id WHERE name LIKE ? OR categories.id = ?", [$search, $category]);
+    }
+    }
 }
 
