68317e18598227ec97eb5207d1b635457a39c556
Leonardo <leo.nicorescu@gmail.com>
2024-04-30 16:19:44 +0200

    Cart finished

--- E-Commerce/app/controllers/CartController.php
+++ E-Commerce/app/controllers/CartController.php
@@ -5,9 +5,14 @@ use App\Models\Cart;
 class CartController extends Controller{
 
     public function index(){
+        $cart = json_decode($_GET['cart'], true);
+        $ids = [];
+        foreach($cart as $item) {
+            $ids[] = $item['id'];
+        }
 
         $model = new Cart($this->getDB());
-        $products = $model->getCart();
+        $products = $model->getCart($ids);
 
         return $this->view('cart.index', compact('products'));
     }
--- E-Commerce/app/models/Cart.php
+++ E-Commerce/app/models/Cart.php
@@ -6,11 +6,18 @@ use database\DBconnection;
 class Cart extends Model
 {
 
-    protected $table ="cart";
+    protected $table ="products";
 
-    public function getCart()
+    public function getCart($ids)
     {
-        return $this->query("SELECT cart.id AS id, name, price, quantity, short_content FROM {$this->table} INNER JOIN product ON cart.id_product = product.id");
+        $products = [];
+        if (empty($ids)) {
+            return $products;
+        }
+        foreach ($ids as $id) {            
+            $products[] = $this->query("SELECT * FROM {$this->table} WHERE id = ?", [$id]);
+        }
+        return $products;
     }
 
     public function changeQuantity(int $id, string $value)
