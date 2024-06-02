5ff64d63afa1b2c15a449f2c7891b97b92978ae7
mathisbachelier <137499136+mathisbachelier@users.noreply.github.com>
2024-06-02 16:51:08 +0200

    Update e_commerce.sql

--- E-Commerce/database/e_commerce.sql
+++ E-Commerce/database/e_commerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 14 mars 2024 à 11:53
+-- Généré le : dim. 02 juin 2024 à 16:15
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.0.30
 
@@ -24,13 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `address`
+-- Structure de la table `addresses`
 --
 
-CREATE TABLE `address` (
+CREATE TABLE `addresses` (
   `id` int(11) NOT NULL,
-  `address` varchar(255) DEFAULT NULL
+  `address` varchar(255) DEFAULT NULL,
+  `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `addresses`
+--
+
+INSERT INTO `addresses` (`id`, `address`, `id_user`) VALUES
+(1, '123 Rue de la Paix', 2),
+(2, '456 Avenue des Champs-Élysées', 3),
+(3, '789 Boulevard Saint-Germain', 1),
+(11, '3 rue de potier', 4),
+(12, '33 rue du Louvre', 6);
 
 -- --------------------------------------------------------
 
@@ -42,6 +54,15 @@ CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Electronics'),
+(2, 'Clothing'),
+(3, 'Books');
 
 -- --------------------------------------------------------
 
@@ -55,20 +76,14 @@ CREATE TABLE `client_address` (
   `id_address` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `discount_coupon`
+-- Déchargement des données de la table `client_address`
 --
 
-CREATE TABLE `discount_coupon` (
-  `id` int(11) NOT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  `discount_value` float DEFAULT NULL,
-  `start_date` date DEFAULT NULL,
-  `end_date` date DEFAULT NULL,
-  `id_user` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `client_address` (`id`, `id_user`, `id_address`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3);
 
 -- --------------------------------------------------------
 
@@ -86,6 +101,16 @@ CREATE TABLE `orders` (
   `date_order` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `orders`
+--
+
+INSERT INTO `orders` (`id`, `order_number`, `status`, `price`, `id_user`, `id_address`, `date_order`) VALUES
+(1, 'ORD123', 1, 100, 1, 1, '2024-03-10 08:30:00'),
+(2, 'ORD456', 2, 200, 2, 2, '2024-03-12 09:45:00'),
+(3, 'ORD789', 1, 150, 6, 3, '2024-03-14 11:00:00'),
+(5, 'ORD33193', 1, 10, 6, 1, '2024-05-02 17:30:31');
+
 -- --------------------------------------------------------
 
 --
@@ -98,6 +123,16 @@ CREATE TABLE `order_products` (
   `id_product` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `order_products`
+--
+
+INSERT INTO `order_products` (`id`, `id_order`, `id_product`, `quantity`) VALUES
+(1, 1, 1, 2),
+(2, 2, 2, 1),
+(3, 3, 3, 3),
+(4, 5, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -116,20 +151,36 @@ CREATE TABLE `products` (
   `url_img` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `content`, `stock`, `price`, `id_category`, `short_content`, `url_img`) VALUES
+(1, 'Laptop', 'A powerful laptop with high specifications.', 10, 1200, 1, 'Powerful laptop', 'laptop.jpg'),
+(2, 'T-Shirt', '100% cotton T-shirt in various colors.', 50, 20, 2, 'Comfortable T-shirt', 'tshirt.jpg'),
+(3, 'Book', 'Bestseller book with captivating content.', 30, 15, 3, 'Bestseller book', 'book.jpg'),
+(4, 'Iphone 15 ', NULL, 12, 1200, 1, 'Iphone 15 Apple', 'iphone15.jpeg');
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `promotions`
+-- Structure de la table `return_product`
 --
 
-CREATE TABLE `promotions` (
+CREATE TABLE `return_product` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `promotion_value` float DEFAULT NULL,
-  `start_date` date DEFAULT NULL,
-  `end_date` date DEFAULT NULL,
-  `id_product` int(11) DEFAULT NULL
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `is_accepted` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `return_product`
+--
+
+INSERT INTO `return_product` (`id`, `order_id`, `product_id`, `description`, `is_accepted`) VALUES
+(1, 1, 1, 'cassé', 0);
 
 -- --------------------------------------------------------
 
@@ -149,13 +200,25 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `last_name`, `first_name`, `role`, `date_of_birth`, `gender`) VALUES
+(1, 'user1@example.com', 'password1', 'Doe', 'John', 2, '1990-01-15', 1),
+(2, 'user2@example.com', 'password2', 'Smith', 'Emma', 2, '1985-05-20', 2),
+(3, 'user3@example.com', 'password3', 'Johnson', 'Michael', 2, '1988-11-10', 1),
+(4, 'theo@gmail.com', 'theo', 'theo', 'béne', 2, '2001-10-18', 0),
+(5, 'admin@gmail.com', '$2y$10$ZggaQtLGurlzjQ8nGbgDFOgReRnG/60EgsBnATOq4a/.6PGKdh4Y2', 'admin', 'admin', 1, '2000-01-01', 0),
+(6, 'im@gmail.com', '$2y$10$bbxejQlz7WD7.Gu/Q7zpUe/JcQuZs0pk812pDFYIpQZfqd36CPfNm', 'Fouide', 'Xavier', 2, '2000-01-01', 0);
+
+--
 -- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `address`
+-- Index pour la table `addresses`
 --
-ALTER TABLE `address`
+ALTER TABLE `addresses`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -168,12 +231,6 @@ ALTER TABLE `categories`
 -- Index pour la table `client_address`
 --
 ALTER TABLE `client_address`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `discount_coupon`
---
-ALTER TABLE `discount_coupon`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -195,9 +252,9 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `promotions`
+-- Index pour la table `return_product`
 --
-ALTER TABLE `promotions`
+ALTER TABLE `return_product`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -211,60 +268,55 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT pour la table `address`
+-- AUTO_INCREMENT pour la table `addresses`
 --
-ALTER TABLE `address`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `addresses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT pour la table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `client_address`
 --
 ALTER TABLE `client_address`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `discount_coupon`
---
-ALTER TABLE `discount_coupon`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `order_products`
 --
 ALTER TABLE `order_products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT pour la table `promotions`
+-- AUTO_INCREMENT pour la table `return_product`
 --
-ALTER TABLE `promotions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `return_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
